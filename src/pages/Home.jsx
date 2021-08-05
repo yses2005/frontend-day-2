e603470316cc26c0cfd3951645cb0505a73f229b
Ian Salazar <iisalazar@up.edu.ph>
2021-08-06 01:58:19 +0800

    task: answered unofficial assignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 import { FullscreenWrapper } from "components";
 import useNewDocumentTitle from "hooks/useNewDocumentTitle";
 
 function Home() {
-  useNewDocumentTitle("Home");
+	const { isLoggedIn } = useSelector((state) => state.auth);
 
-  return (
-    <FullscreenWrapper>
-      Home <Link to="/login">Login</Link>
-    </FullscreenWrapper>
-  );
+	useNewDocumentTitle("Home");
+
+	return (
+		<FullscreenWrapper>
+			Home <Link to="/login">Login</Link>
+			{isLoggedIn && "Welcome! You are logged in"}
+		</FullscreenWrapper>
+	);
 }
 
 export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,22 +5,33 @@ import { Link } from "react-router-dom";
 import { FullscreenWrapper } from "components";
 
 import {
-  login as loginUser,
-  logout as logoutUser,
+	login as loginUser,
+	logout as logoutUser,
 } from "redux/actions/authActions";
 
 function Login() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const loginPending = useSelector((state) => state.auth.loginPending);
-  const loginError = useSelector((state) => state.auth.loginError);
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+	const loginPending = useSelector((state) => state.auth.loginPending);
+	const loginError = useSelector((state) => state.auth.loginError);
+	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-  return (
-    <FullscreenWrapper>
-      Login <Link to="/">Home</Link>
-    </FullscreenWrapper>
-  );
+	return (
+		<FullscreenWrapper>
+			Login <Link to="/">Home</Link>
+			{isLoggedIn ? (
+				<button onClick={() => dispatch(logoutUser())} disabled={loginPending}>
+					Logout
+				</button>
+			) : (
+				<button onClick={() => dispatch(loginUser())} disabled={loginPending}>
+					Login
+				</button>
+			)}
+			{isLoggedIn && "Welcome! You are logged in"}
+			{loginError && <p style={{ color: "red" }}>{loginError}</p>}
+		</FullscreenWrapper>
+	);
 }
 
 export default Login;
